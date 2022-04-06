8e1487559bf4346fba973de9825277da30076e40
MShadowHUN <b0totdav@vasvari.org>
2022-04-06 14:38:32 +0200

    a

--- 10 oop/Feladat 2/main.py
+++ 10 oop/Feladat 2/main.py
@@ -1,7 +1,6 @@
 from motor import Motorkerekpar
 from tulajdonos import Tulajdonos
 
-motor:Motorkerekpar=Motorkerekpar("Honda", "CBF 600", "Naked",57,600)
-print(motor)
-
 tulajdonos:Tulajdonos=Tulajdonos("Toth David", "2005.06.24", "férfi", "Szeged")
+motor:Motorkerekpar=Motorkerekpar("Honda", "CBF 600", "Naked",57,600,198 ,tulajdonos)
+print(motor)
--- 10 oop/Feladat 2/motor.py
+++ 10 oop/Feladat 2/motor.py
@@ -1,13 +1,16 @@
+from tulajdonos import Tulajdonos
+
 class Motorkerekpar:
-    def __init__(self, gyarto:str="", tipus:str="", kivitel:str="", teljesitmeny:int=0, hengerurtartalom:int=0, suly:int=0 ):
+    def __init__(self, gyarto:str, tipus:str, kivitel:str, teljesitmeny:int, hengerurtartalom:int, suly:int, tulajdonos:Tulajdonos):
         super(). __init__()
         self.gyarto:str=gyarto
         self.tipus:str=tipus
         self.kivitel:str=kivitel
-        self.teljesitmeny:int=gyarto
+        self.teljesitmeny:int=teljesitmeny
         self.hengerurtartalom:int=hengerurtartalom
         self.suly:int=suly
-    
+        self.tulajdonos:Tulajdonos=tulajdonos
+
     def __str__(self):
-        return f"{self.gyarto} {self.tipus} {self.kivitel} ({self.teljesitmeny}kW {self.hengerurtartalom})cm^3"
+        return f"{self.gyarto} {self.tipus} {self.kivitel} ({self.teljesitmeny}kW {self.hengerurtartalom})cm^3\n Száraz Tömege:{self.suly}kg"
 
