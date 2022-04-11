4a69d6bf66615c5c795286b84b50745579e1a06d
MShadowHUN <b0totdav@vasvari.org>
2022-04-11 10:26:03 +0200

    a

--- 10 oop/Feladat 4/csoport.py
+++ 10 oop/Feladat 4/csoport.py
@@ -1,10 +1,15 @@
 from tanulok import Tanulo
+from typing import *
 
 class Csoport:
     def __init__(self, csoportnev:str, tanulok:List[Tanulo]):
-        super() __init__()
+        super(). __init__()
         self.csoportnev:str=csoportnev
         self.tanulok:List[Tanulo]=tanulok
     
     def __str__(self):
-        return f"{self.csoportnev}\n{self.tanulok}"
+        output=f"{self.csoportnev}\n"
+        for tanulo in self.tanulok:
+            output +=f"\t - {str(tanulo)}\n"
+
+        return output
--- 10 oop/Feladat 4/main.py
+++ 10 oop/Feladat 4/main.py
@@ -3,6 +3,12 @@ from csoport import Csoport
 from typing import *
 
 elsotanulo:Tanulo=Tanulo("Hétfő","Henrik","1997.07.14")
-masodiktanulo:Tanulo=Tanulo("Hétfő","Henrik","1997.07.14")
+masodiktanulo:Tanulo=Tanulo("Kedd","Kevin","1997.06.24")
+harmadiktanulo:Tanulo=Tanulo("Szerda","Szilárd","1997.07.04")
 tanulok:List[Tanulo]=[]
+tanulok.append(elsotanulo)
+tanulok.append(masodiktanulo)
+tanulok.append(harmadiktanulo)
 
+csoport:Csoport=Csoport("10.b info", tanulok)
+print(csoport)
